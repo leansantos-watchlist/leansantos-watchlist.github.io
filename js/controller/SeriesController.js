7c4f07e05383e8b5a97f78eed86eb694d549f294
leansantos <lesteranthony_s@yahoo.com.ph>
2015-07-15 08:17:29 +0800

    DFS

--- js/controller/SeriesController.js
+++ js/controller/SeriesController.js
@@ -2,5 +2,25 @@ app.controller('SeriesController', ['$scope', '$http', function($scope, $http){
 	$http.get(seriesdata)
 		.success(function(response){
 			$scope.tvseries = response.series;
+			
 		});
+
+
+	// $http.get(tvseriesdata)
+	// 	.success(function(response){
+	// 		$scope.testing = response.tvseries;
+	// 		$scope.season = response.tvseries[0].season;
+	// 		$scope.episode = response.tvseries[0].season[1].episode;
+
+	// 		$scope.seasonid = "";
+	// 		console.log(response.tvseries[0].season)
+			
+
+	// 		$scope.tvseries = function(id){
+	// 			return response.tvseries[id].id;
+	// 		};
+	// 		$scope.season = function(id){
+	// 			return response.tvseries[id].id;
+	// 		}
+	// 	});
 }])
